e0487ed712d9dbcec7bc8ca7c408ca51649b74a9
bilel <bilelh532@gmail.com>
2019-05-06 17:14:39 +0200

    SupprimerPizza DONE

--- src/SupprimerPizzaService.java
+++ src/SupprimerPizzaService.java
@@ -1,9 +1,30 @@
+import java.util.Scanner;
+
+import fr.pizzeria.model.Pizza;
 
 public class SupprimerPizzaService extends MenuService {
+	
+	Scanner questionUser = new Scanner(System.in);
 
 	@Override
-	public void executeUC() {
+	public void executeUC(PizzaMemoDao pizzaDao) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("Suppression d'une pizza ");
+		
+		
+		System.out.println("Veuillez choisir le code de la pizza à supprimer:  ");
+		questionUser.nextLine();
+		String suppr_code = questionUser.nextLine();
+		
+		boolean exist = pizzaDao.pizzaExists(suppr_code) ;
+		
+		
+		if (exist = true) {
+		
+			pizzaDao.deletePizza(suppr_code);
+		
+		}
 		
 	}
 
