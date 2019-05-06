ecfebef6c997a43cf3b381d469f04cd0053feb0f
bilel <bilelh532@gmail.com>
2019-05-06 17:17:24 +0200

    Ajout d'un message dans ListerPizza

--- src/ListerPizzaService.java
+++ src/ListerPizzaService.java
@@ -6,6 +6,8 @@ public class ListerPizzaService extends MenuService {
 	public void executeUC(PizzaMemoDao pizzaDao) {
 		// TODO Auto-generated method stub
 		
+		System.out.println("Liste des pizzas  ");
+		
 		for ( int i = 0 ; i < pizzaDao.findAllPizzas().length ; i++) {
 			
 			System.out.println(pizzaDao.findAllPizzas() [i].id + " _ " + pizzaDao.findAllPizzas() [i].code + " -> " + pizzaDao.findAllPizzas() [i].libelle + " ( " + String.format("%.2f" , pizzaDao.findAllPizzas() [i].prix) + "€ )");
