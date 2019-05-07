d01ead0e64d03458e8a7264037dd96c2fed119da
bilel <bilelh532@gmail.com>
2019-05-07 12:24:06 +0200

    MenuServiceFactory renseigné

--- src/fr/pizzeria/factory/MenuServiceFactory.java
+++ src/fr/pizzeria/factory/MenuServiceFactory.java
@@ -1,5 +1,55 @@
 package fr.pizzeria.factory;
 
-public class MenuServiceFactory {
+import fr.pizzeria.service.AjouterPizzaService;
+import fr.pizzeria.service.ListerPizzaService;
+import fr.pizzeria.service.MenuService;
+import fr.pizzeria.service.ModifierPizzaService;
+import fr.pizzeria.service.PizzaMemoDao;
+import fr.pizzeria.service.SupprimerPizzaService;
 
+public class MenuServiceFactory {
+	
+	public MenuService factory (int user_choice) {
+		
+		
+		PizzaMemoDao pizza = new PizzaMemoDao() ;
+		pizza.initialisation();
+		
+		MenuService liste = new ListerPizzaService () ;
+		MenuService ajout = new AjouterPizzaService () ;
+		MenuService modif = new ModifierPizzaService () ;
+		MenuService suppr = new SupprimerPizzaService () ;
+		MenuService choice = null ;
+		
+		while (user_choice != 99) {
+				
+						// CHOIX 1. AFFICHAGE DE LA LISTE DE PIZZAS
+			if (user_choice == 1) {     
+			
+			choice = liste ;
+						
+						// CHOIX 2. AJOUT D'UNE PIZZA
+			} else if ( user_choice == 2) { 
+			
+			choice = ajout ;
+			
+						// CHOIX 3. MODIFICATION D'UNE PIZZA
+			} else if ( user_choice == 3) {
+			
+			choice = modif ;
+			
+						// CHOIX 4. SUPPRESSION D'UNE PIZZA
+			} else if ( user_choice == 4) {
+			
+			choice = suppr ;
+			
+						// MESSAGE POUR LE CAS OU L'OPTION SAISIE N'EST PAS DISPONIBLE
+			} else {
+			
+			System.out.println("Je n'ai pas compris, Veuillez recommencer ");
+			
+			}
+		}
+		return choice ;
+	}
 }
